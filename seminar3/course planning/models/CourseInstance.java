706d5a12d49bd537b3d658266cfb61da5b26837f
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-12-10 11:37:51 +0100

    Update CourseInstance.java

--- seminar3/course planning/models/CourseInstance.java
+++ seminar3/course planning/models/CourseInstance.java
@@ -1,1 +1,20 @@
+package model;
 
+public class CourseInstance {
+    public int instanceId;
+    public String courseCode;
+    public String courseName;
+    public String period;
+    public int year;
+    public int numStudents;
+
+    public CourseInstance(int instanceId, String courseCode, String courseName,
+            String period, int year, int numStudents) {
+        this.instanceId = instanceId;
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.period = period;
+        this.year = year;
+        this.numStudents = numStudents;
+    }
+}
