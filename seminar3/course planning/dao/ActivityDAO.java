5f4794de22f1c93072ae1b5d6f690734020ac298
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-12-10 15:56:45 +0100

    Update ActivityDAO.java

--- seminar3/course planning/dao/ActivityDAO.java
+++ seminar3/course planning/dao/ActivityDAO.java
@@ -4,14 +4,14 @@ public class ActivityDAO {
   
     public int ensureActivity(Connection co, String name, double factor) throws SQLException {
         String find = "SELECT activitytype_id FROM ActivityType WHERE activity_name = ?";
-        try (Statement s = co.prepareStatement(find)) {
+        try (Statement s = co.Statements(find)) {
             s.setString(1, name);
             try (ResultSet rs = s.executeQuery()) {
                 if (rs.next())
                     return rs.getInt(1);
             }  }
         String ins = "INSERT INTO ActivityType(activity_name, factor) VALUES (?,?) RETURNING activitytype_id";
-        try (Statement s = conn.prepareStatement(ins)) {
+        try (Statement s = conn.Statements(ins)) {
             s.setString(1, name);
             s.setDouble(2, factor);
             try (ResultSet rs = s.executeQuery()) {
@@ -21,7 +21,7 @@ public class ActivityDAO {
     public void upsertPlannedActivity(Connection co, int instanceId, int activityId, double hours)
             throws SQLException {
         String sql = "INSERT INTO PlannedActivity(courseinstance_id, activitytype_id, planned_hours) VALUES (?,?,?) ON CONFLICT (courseinstance_id, activitytype_id) DO UPDATE SET planned_hours = EXCLUDED.planned_hours";
-        try (Statement s = co.prepareStatement(sql)) {
+        try (Statement s = co.Statements(sql)) {
             s.setInt(1, instanceId);
             s.setInt(2, activityId);
             s.setDouble(3, hours);
