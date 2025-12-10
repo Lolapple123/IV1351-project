7c0b5dc19024fb35b6475a5e463d2a1ad37111b8
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-12-10 11:39:36 +0100

    Update CostTeaching.java

--- seminar3/course planning/models/CostTeaching.java
+++ seminar3/course planning/models/CostTeaching.java
@@ -1,1 +1,24 @@
+package model;
 
+public class CostTeaching {
+    public int instanceId;
+    public String courseCode;
+    public String period;
+    public int year;
+    public double plannedHours;
+    public double actualHours;
+    public double plannedKsek;
+    public double actualKsek;
+
+    public CostTeaching(int instanceId, String courseCode, String period, int year,
+            double plannedHours, double actualHours, double plannedKsek, double actualKsek) {
+        this.instanceId = instanceId;
+        this.courseCode = courseCode;
+        this.period = period;
+        this.year = year;
+        this.plannedHours = plannedHours;
+        this.actualHours = actualHours;
+        this.plannedKsek = plannedKsek;
+        this.actualKsek = actualKsek;
+    }
+}
