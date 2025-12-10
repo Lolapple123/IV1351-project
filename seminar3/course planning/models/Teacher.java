11e0060432d4c05b14e2c0bfda5641da88a53178
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-12-10 11:38:57 +0100

    Update Teacher.java

--- seminar3/course planning/models/Teacher.java
+++ seminar3/course planning/models/Teacher.java
@@ -1,1 +1,27 @@
+package model;
 
+public class Teacher {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String designation;
+
+    public Teacher(int id, String firstName, String lastName, String designation) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.designation = designation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+}
