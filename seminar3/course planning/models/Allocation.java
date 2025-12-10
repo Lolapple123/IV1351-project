2534279d6c0ca55f3c12c7ab3d618c616c476286
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-12-10 11:37:04 +0100

    Update Allocation.java

--- seminar3/course planning/models/Allocation.java
+++ seminar3/course planning/models/Allocation.java
@@ -1,1 +1,15 @@
+package model;
 
+public class Allocation {
+    public String instanceId;
+    public int teacherId;
+    public String activity;
+    public int hours;
+
+    public Allocation(String instanceId, int teacherId, String activity, int hours) {
+        this.instanceId = instanceId;
+        this.teacherId = teacherId;
+        this.activity = activity;
+        this.hours = hours;
+    }
+}
