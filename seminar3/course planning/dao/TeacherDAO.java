33323585162f0995ea5d6f530c85a11a16051c66
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-12-10 16:06:56 +0100

    Update TeacherDAO.java

--- seminar3/course planning/dao/TeacherDAO.java
+++ seminar3/course planning/dao/TeacherDAO.java
@@ -1,1 +1,23 @@
+package dao;
+import model.Teacher;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TeacherDAO {
+    public List<Teacher> getAllTeachers() {
+        List<Teacher> list = new ArrayList<>();
+        String sql = "SELECT emp_id, first_name, last_name";
+        try (Connection c = DATABASEconnect.getConnection();
+                Statement statement = c.createStatement();
+                ResultSet result = statement.executeQuery(sql)) {
+            while (result.next()) {
+                list.add(new Teacher(
+                        result.getInt("emp_id"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("designation")));
+            } } catch (Exception e) {
+            System.out.println("Error loading teachers: " + e.getMessage()); }
+        return list;
+    } }
